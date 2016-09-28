a1b5f914b74f1f1ece708d65a0593ada699290ed
Chong Shao <cshao@cs.unc.edu>
2016-09-28 13:51:10 -0400

    update

--- disparity_test.m
+++ disparity_test.m
@@ -6,7 +6,25 @@ im4 = imread('im4.png');
 im7 = imread('im7.png');
 im8 = imread('im8.png');
 
-disparityMap = compute_disparity(im3, im4);
+disparity_map = compute_disparity(im3, im4);
+binary_disparity_map = threshold_disparity(disparity_map);
+refined_disparity_map = refine_dispairty(binary_disparity_map);
+
+imshow(refined_disparity_map);
+[encoded, data_length] = encode_map(refined_disparity_map, im3);
+
+decoded_image = decode_map(encoded); 
+
+quality = quality_meature(im3, decoded_image);
+
+
+
+
+
+
+
+
+
 
 
 
--- retrieve_image.m
+++ retrieve_image.m
@@ -1,6 +1,5 @@
 % recovers the image from the input map
 function [ output_image ] = retrieve_image( input_map )
 
-
 end
 
